eb3d65faf455a03deec21b71a6a4efa1191cc483
Nadya <db.javaschool@gmail.com>
2016-11-14 10:15:11 +0300

    debugging and comments

--- src/main/java/com/db/edu/PrinterApp.java
+++ src/main/java/com/db/edu/PrinterApp.java
@@ -6,8 +6,20 @@ import org.slf4j.LoggerFactory;
 public class PrinterApp {
     private static final Logger logger = LoggerFactory.getLogger(PrinterApp.class);
 
+    /**
+     * @throws
+     * @param args
+     */
     public static void main(String... args) {
         logger.debug("Entering main method with args {}", args);
-        System.out.println( "Hello World!!!" );
+        System.out.println( "Hello World!!!" + args[0]);
+        System.out.println( "Hello World!!!" + args[1]);
+        mySuperMethod();
+        System.out.println( "Hello World!!!" + args[2]);
+        System.out.println( "Hello World!!!" + args[0]);
+    }
+
+    public static  void mySuperMethod() {
+        System.out.print("Test print");
     }
 }
